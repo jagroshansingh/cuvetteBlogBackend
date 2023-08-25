44ebaaf56495dc3799233051c6bc03f9a8019ae6
Jagroshan <jagroshansingh615@gmail.com>
2023-08-25 14:55:47 +0530

    changed localhost to aws

--- db.js
+++ db.js
@@ -2,9 +2,9 @@ const mysql=require('mysql2')
 require('dotenv').config()
 
 const connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'JRS615@yahoo',
+    host:'database-1.cl6c5dt3dv1u.eu-north-1.rds.amazonaws.com',
+    user:'admin',
+    password:`${process.env.dbPassword}`,
     database:'cuvetteblogassignment'
 })
 
